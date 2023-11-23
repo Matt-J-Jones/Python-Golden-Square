24e0d4e3b1e73310902917d48e5240d5508674c5
Matt-J-Jones <aratinlondon@gmail.com>
2023-11-23 07:56:43 +0000

    exercise complete - making snippets

--- 2 phase-two/test-drive-single-function/lib/make_snippet.py
+++ 2 phase-two/test-drive-single-function/lib/make_snippet.py
@@ -1,3 +1,8 @@
-def make_snippet():
+def make_snippet(str):
+    str_split = str.split()
+    if len(str_split) <= 5:
+        return str
+    elif len(str_split) > 5:
+        return " ".join(str_split[0:5]) + "..."
     return None
   
--- 2 phase-two/test-drive-single-function/tests/test_make_snippet.py
+++ 2 phase-two/test-drive-single-function/tests/test_make_snippet.py
@@ -1,4 +1,13 @@
 from lib.make_snippet import make_snippet
 
 def test_returns_value():
-    assert make_snippet() != None
+    assert make_snippet("") is not None
+
+def test_return_string_of_2_words():
+    assert make_snippet("Hello, World") == "Hello, World"
+
+def test_return_string_of_5_words():
+    assert make_snippet("This is five words long") == "This is five words long"
+  
+def test_return_shortened_string():
+    assert make_snippet("there is a spectre haunting europe") == "there is a spectre haunting..."
