4cdbca20a7ba9374636b67d1bba659a3d75c23ab
Matt-J-Jones <aratinlondon@gmail.com>
2023-11-30 12:48:50 +0000

    Project Set Up

--- 3_phase_three/4_unit_testing_api_requests/lib/time_error.py
+++ 3_phase_three/4_unit_testing_api_requests/lib/time_error.py
@@ -2,6 +2,8 @@ import time
 import requests
 
 class TimeError:
+    def __init__(self, requester):
+        self._requester = requester
     # Returns difference in seconds between the time on an external server
     # and the time on this computer
     def error(self):
@@ -11,6 +13,6 @@ class TimeError:
     # outside. You also shouldn't stub it in your tests. So if your tests contain
     # the words `get_server_time`, you're on the wrong track.
     def _get_server_time(self):
-        response = requests.get("https://worldtimeapi.org/api/ip")
+        response = self._requester.get("https://worldtimeapi.org/api/ip")
         json = response.json()
         return json["unixtime"]
--- 3_phase_three/4_unit_testing_api_requests/tests/test_unit_time_error.py
+++ 3_phase_three/4_unit_testing_api_requests/tests/test_unit_time_error.py
@@ -1,5 +1,29 @@
 from lib.time_error import TimeError
+import requests
+from unittest.mock import Mock
 
 def test_creates_class_object():
     timeerror = TimeError()
     assert isinstance(timeerror, TimeError)
+
+def test_mocks_server_time_get():
+    requster_mock = Mock(name = "requester")
+    response_mock = Mock(name = "response")
+    requster_mock.get.return_value = response_mock
+    response_mock.json.return_value = {"abbreviation":"GMT",
+                                        "client_ip":"158.41.48.3",
+                                        "datetime":"2023-11-28T15:01:49.508080+00:00",
+                                        "day_of_week":2,
+                                        "day_of_year":332,
+                                        "dst":False,
+                                        "dst_from":None,
+                                        "dst_offset":0,
+                                        "dst_until":None,
+                                        "raw_offset":0,
+                                        "timezone":"Europe/London",
+                                        "unixtime":1701183709,
+                                        "utc_datetime":"2023-11-28T15:01:49.508080+00:00",
+                                        "utc_offset":"+00:00",
+                                        "week_number":48}
+    timeerror = TimeError(requster_mock)
+    assert timeerror.error() == 0.1
