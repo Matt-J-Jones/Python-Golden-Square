fec92ac0f7bc3c1b4f54a1d72fedd86101caf0ac
Matt-J-Jones <aratinlondon@gmail.com>
2023-11-30 16:05:51 +0000

    Unit Test - Menu Item complete

--- 4_takeaway_project/lib/menu_item.py
+++ 4_takeaway_project/lib/menu_item.py
@@ -1,3 +1,25 @@
 class MenuItem:
-    def __init__(self):
-        pass
+    def __init__(self, name, price, availability):
+        self.name = name
+        self.price = price
+        self.availability = availability
+
+    def return_formatted_item(self):
+        return f"{self.name}: £{self.return_price_as_string()}, {self.return_status()}"
+
+    def mark_sold_out(self):
+        self.availability = False
+
+    def return_price_as_string(self):
+        temp_price = str(self.price).split(".")
+        if len(temp_price[1]) == 1:
+            return str(temp_price[0] + "." + temp_price[1] + "0")
+        return str(".".join(temp_price))
+
+    def returns_name(self):
+        return self.name
+
+    def return_status(self):
+        if self.availability:
+            return "Available"
+        return "Sold Out"
--- 4_takeaway_project/tests/test_unit_menu_item.py
+++ 4_takeaway_project/tests/test_unit_menu_item.py
@@ -2,5 +2,29 @@ from lib.menu_item import MenuItem
 import pytest
 
 def test_creates_object():
-    menu_item = MenuItem()
+    menu_item = MenuItem("example item", 9.99, True)
     assert isinstance(menu_item, MenuItem)
+
+def test_returns_formatted_item_available():
+    menu_item = MenuItem("example item", 9.99, True)
+    assert menu_item.return_formatted_item() == "example item: £9.99, Available"
+
+def test_returns_formatted_item_unavailable():
+    menu_item = MenuItem("example item", 9.99, False)
+    assert menu_item.return_formatted_item() == "example item: £9.99, Sold Out"
+
+def test_adds_item_marks_sold_out():
+    menu_item = MenuItem("example item", 9.99, True)
+    assert menu_item.return_formatted_item() == "example item: £9.99, Available"
+    menu_item.mark_sold_out()
+    assert menu_item.return_formatted_item() == "example item: £9.99, Sold Out"
+
+def test_returns_formatted_item_with_2_decimal_places():
+    menu_item = MenuItem("example item", 9.00, False)
+    assert menu_item.return_price_as_string() == "9.00"
+
+def test_returns_item_name_price_availability_individually():
+    menu_item = MenuItem("example item", 1.01, False)
+    assert menu_item.return_price_as_string() == "1.01"
+    assert menu_item.returns_name() == "example item"
+    assert menu_item.return_status() == "Sold Out"
